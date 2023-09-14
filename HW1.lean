66dc1c9b77a4dd93137685288476fead1f19be1c
teona6 <113918262+teona6@users.noreply.github.com>
2023-09-14 04:19:37 +0000

    Update HW1.lean

--- HW1.lean
+++ HW1.lean
@@ -17,7 +17,6 @@ example {a b : ℤ } (h1 : a = 2 * b + 5) (h2 : b = 3) : a = 11 :=
   calc
     a = 2 * b + 5 := by rw [h1]
     _ = 2 * 3 + 5 := by rw [h2]
-    _ = 6 + 5 := by ring
     _ = 11 := by ring
 
 -- 4.2
