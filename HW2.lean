6591c8ac12763db2a632acbf01571b92c87921e7
bagashvilit <bagashvilit@allegheny.edu>
2023-09-20 23:05:32 -0400

    add hw2

--- HW2.lean
+++ HW2.lean
@@ -4,15 +4,13 @@ import Library.Tactic.Numbers
 import Library.Tactic.Addarith
 import Library.Tactic.Cancel
 
-theorem example_v1 {x y : ℤ} (h1 : x + 3 ≥ 2 * y) (h2 : 1 ≤ y) : x ≥ -1 := by
+-- Problem 6a
+example {x y : ℤ} (h1 : x + 3 ≥ 2 * y) (h2 : 1 ≤ y) : x ≥ -1 :=
   calc
-    x = x + 3 - 3 := by ring
-    _ = (2 * y - 3) + 3 - 3 := by rel [h1]
-    _ = y + y - 3 := by ring
-    _ = y + (1 - 3) := by rel [h2]
-    _ = y + (-2) := by ring
-    _ ≥ 1 + (-2) := by rel [h2]
-    _ = -1 := by ring
+    x = (x + 3) - 3 := by ring
+    _ ≥ 2 * y - 3 := by rel [h1]
+    _ ≥ 2 * 1 - 3 := by rel [h2]
+    _ ≥ -1 := by numbers
 
 
 theorem example_v2 {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥ 3 := by
@@ -23,9 +21,4 @@ theorem example_v2 {a b : ℚ} (h1 : 3 ≤ a) (h2 : a + 2 * b ≥ 4) : a + b ≥
     _ ≥ 3 - 1 + 1 := by rel [h2]
     _ = 3 := by ring
 
-theorem six_c_v2 {x : ℤ} (h1 : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 := by
-  calc
-    x ^ 3 - 8 * x ^ 2 + 2 * x = x * (x * x - 8 * x + 2) := by ring
-    _ = x * (x - 4) * (x - 4) + 14 * x - 2 * 7 * x := by ring
-    _ ≥ 9 * (9 - 4) * (9 - 4) + 14 * 9 - 2 * 7 * 9 := by rel [h1]
-    _ = 3 := by numbers
+
